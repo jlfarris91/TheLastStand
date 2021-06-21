3d457bc4249c0811642763f9b0a75710e0b1683c
James Farris <jlfarris91@gmail.com>
2021-06-21 13:02:49 -0700

    RunPipeline.ps1 for cync

--- build/RunPipeline.ps1
+++ build/RunPipeline.ps1
@@ -6,20 +6,20 @@ $OutputWurstSourceMapFilePath = $env:WurstSourceMapFilePath
 $IntermediateDirPath = [System.IO.Path]::Combine($env:TempRoot, "maps")
 $OutputSpawnRegionScriptFile = [System.IO.Path]::Combine($env:WurstRoot, "Spawning\SpawnRegionInit.wurst")
 
-# When building locally (specifically on Jame's machine, for now) we can reference the
-# dumped casc data directly
-if ($env:IsLocalBuild -eq 1)
-{
-  $War3ArchiveDirPath = "D:\Projects\WarcraftIII\MPQ\Dump\war3.w3mod"
-  $OutputListFilePath = [System.IO.Path]::Combine($env:BuildRoot, "listfile")
-}
-# On the build machine we want to use only the slimmest data we can to improve build times
-# and save on space so we need to use our cached, checked-in version of the casc data
-# This data is cached via BuildSlimArchive.ps1 and should be updated with each War3 patch release
-else
-{
+## When building locally (specifically on Jame's machine, for now) we can reference the
+## dumped casc data directly
+#if ($env:IsLocalBuild -eq 1)
+#{
+#  $War3ArchiveDirPath = "D:\Projects\WarcraftIII\MPQ\Dump\war3.w3mod"
+#  $OutputListFilePath = [System.IO.Path]::Combine($env:BuildRoot, "listfile")
+#}
+## On the build machine we want to use only the slimmest data we can to improve build times
+## and save on space so we need to use our cached, checked-in version of the casc data
+## This data is cached via BuildSlimArchive.ps1 and should be updated with each War3 patch release
+#else
+#{
   $War3ArchiveDirPath = [System.IO.Path]::Combine($env:BuildRoot, "war3.wmod")
-}
+#}
 
 & $ExeFilePath "$SourceMapDirPath" "$OutputWurstSourceMapFilePath" "$IntermediateDirPath" "$OutputSpawnRegionScriptFile" "$War3ArchiveDirPath" "$OutputListFilePath"
 
